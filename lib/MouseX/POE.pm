9f35d7acded9b8d1f32ef1033284d358c8c2c01e
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-05-13 19:17:45 +0100

    Amend the documentation to remove mention of MouseX::Declare

--- lib/MouseX/POE.pm
+++ lib/MouseX/POE.pm
@@ -72,34 +72,6 @@ __END__
     Counter->new();
     POE::Kernel->run();
 
-or with L<MouseX::Declare|MouseX::Declare>:
-
-    class Counter {
-        use MouseX::POE::SweetArgs qw(event);
-
-        has count => (
-            isa     => 'Int',
-            is      => 'rw',
-            lazy    => 1,
-            default => sub { 0 },
-        );
-
-        sub START {
-            my ($self) = @_;
-            $self->yield('increment')
-        }
-
-        event increment => sub {
-            my ($self) = @_;
-            print "Count is now " . $self->count . "\n";
-            $self->count( $self->count + 1 );
-            $self->yield('increment') unless $self->count > 3;
-        }
-    }
-
-    Counter->new();
-    POE::Kernel->run();
-
 =head1 DESCRIPTION
 
 MouseX::POE is a L<Mouse> wrapper around a L<POE::Session>.
@@ -114,7 +86,7 @@ Create an event handler named $name.
 
 Default POE-related methods are provided by L<MouseX::POE::Meta::Trait::Object|MouseX::POE::Meta::Trait::Object>
 which is applied to your base class (which is usually L<Mouse::Object|Mouse::Object>) when
-you use this module. See that module for the documentation for. Below is a list
+you use this module. See that module for the documentation for it. Below is a list
 of methods on that class so you know what to look for:
 
 =method get_session_id
@@ -151,37 +123,6 @@ A cheap alias for the same POE::Kernel function which will gurantee posting to t
 =method STARTALL
 
 =method STOPALL
-
-=head1 NOTES ON USAGE WITH L<MouseX::Declare>
-
-L<MouseX::Declare|MouseX::Declare> support is still "experimental". Meaning that I don't use it,
-I don't have any code that uses it, and thus I can't adequately say that it
-won't cause monkeys to fly out of any orifices on your body beyond what the
-tests and the SYNOPSIS cover.
-
-That said there are a few caveats that have turned up during testing.
-
-1. The C<method> keyword doesn't seem to work as expected. This is an
-integration issue that is being resolved but I want to wait for
-L<MouseX::Declare|MouseX::Declare> to gain some more polish on their slurpy
-arguments.
-
-2. MouseX::POE attempts to re-export L<Mouse>, which
-L<MouseX::Declare> has already exported in a custom fashion.
-This means that you'll get a keyword clash between the features that
-L<MouseX::Declare|MouseX::Declare> handles for you and the features that Mouse
-handles. To work around this you'll need to write:
-
-    use MouseX::POE qw(event);
-    # or
-    use MouseX::POE::SweetArgs qw(event);
-    # or
-    use MouseX::POE::Role qw(event);
-
-to keep MouseX::POE from exporting the sugar that
-L<MouseX::Declare|MouseX::Declare> doesn't like. This is fixed in the Git
-version of L<MouseX::Declare|MouseX::Declare> but that version (as of this
-writing) is not on the CPAN.
 
 =head1 SEE ALSO
 
